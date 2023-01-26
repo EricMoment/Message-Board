14af3eb3f0f0c1b1a61771de9db73de73044b583
Eric <a365900630@gmail.com>
2023-01-27 00:22:34 +0900

    back to using express session

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 
-const session = require('cookie-session');
+const session = require("express-session");
 const passport = require("./passport.js");
 
 const mongoose = require("mongoose");
